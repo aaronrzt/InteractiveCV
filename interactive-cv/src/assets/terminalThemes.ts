a462ecef85124a10feb12397dd2d5ae8a29ef394
aaronrzt <aaronrzt@outlook.com>
2025-11-30 13:46:02 -0600

    [add] Custom themes

---
+++ interactive-cv/src/assets/terminalThemes.ts
@@ -0,0 +1,26 @@
+// Ubuntu-like theme
+export const ubuntuTheme = {
+  background: '#181818',
+  foreground: '#EEEEEE',
+  cursor: '#FFFFFF',
+  black:   '#2E3436',
+  red:     '#CC0000',
+  green:   '#4E9A06',
+  yellow:  '#C4A000',
+  blue:    '#3465A4',
+  magenta: '#75507B',
+  cyan:    '#06989A',
+  white:   '#D3D7CF',
+  brightBlack:   '#555753',
+  brightRed:     '#EF2929',
+  brightGreen:   '#8AE234',
+  brightYellow:  '#FCE94F',
+  brightBlue:    '#729FCF',
+  brightMagenta: '#AD7FA8',
+  brightCyan:    '#34E2E2',
+  brightWhite:   '#EEEEEC'
+};
+
+export const themes = {
+  ubuntu: ubuntuTheme
+};
